a3c3b1ab2a8eca26bda62f9ad034f5d1b2ec2f9c
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-03-07 11:28:03 -0300

    alteações

--- form/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ form/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
-function FormularioCadastro(){
-    return(
-        <form>
-            <input type="text"/>
-        </form>
-    );
+function FormularioCadastro() {
+  return (
+    <form>
+       <TextField id="Nome" label="Nome" variant="outlined" fullWidth />
+       <TextField id="Sobrenome" label="Sobrenome" variant="outlined" fullWidth />
+       <TextField id="Cpf" label="Cpf" variant="outlined" fullWidth />
+
+        <label>Promoções</label>       
+        <input type="checkbox"/>
+        <label>Novidades</label>   
+        <input type="checkbox"/>
+              
+      <Button variant="contained" color="primary" type="submit" fullWidth>Entrar</Button>
+     
+    </form>
+  );
 }
 
 export default FormularioCadastro;
