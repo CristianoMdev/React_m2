1f554f6c5672e32a13eebfa8831d242438f0e963
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-03-11 21:34:41 -0300

    final M2

--- form/src/App.js
+++ form/src/App.js
@@ -8,11 +8,22 @@ class App extends Component {
   render(){
     return (
       <Container component="article" maxWidth="xs">
-        <Typography variant="h4" component="h1" align="center" color="primary">Formulário Cadastro</Typography>
-        <FormularioCadastro />
+        <Typography variant="h4" component="h1" align="center" color="primary">Formulário de Cadastro</Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
       </Container>
   );
  }
+}
+function aoEnviarForm(dados){
+  console.log(dados)
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos!"}
+  }else{
+    return{valido:true, texto:""}
+  }
 }
 
 export default App;
