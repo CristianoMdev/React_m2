bac7858f6d0a8da226698987ec78c81132fbc631
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-03-26 17:04:36 -0300

    Steps

--- form/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ form/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,23 +1,42 @@
-import { Typography } from '@material-ui/core';
-import React, { useState } from 'react';
+import { Typography, Stepper, Step, StepLabel  } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
 const [etapaAtual, setEtapaAtual ] = useState(0);
+const [dadosColetados, setDados] = useState({});
+
+useEffect(() =>{
+  if(etapaAtual === formularios.length){
+    aoEnviar(dadosColetados)
+  }
+})
 
 const formularios = [
-  <DadosUsuario aoEnviar= {proximo} />,
-  <DadosPessoais  aoEnviar={proximo} validarCPF={validarCPF} />,
-  <DadosEntrega aoEnviar={aoEnviar} />
+  <DadosUsuario aoEnviar = {coletarDados} />,
+  <DadosPessoais  aoEnviar = {coletarDados} validarCPF={validarCPF} />,
+  <DadosEntrega aoEnviar = {coletarDados} />,
+  <Typography variant="h5" align="center" color="primary">Obrigado por se cadastrar!</Typography>
 ];
 
-function proximo(dados){
+function coletarDados(dados){
+  setDados({...dadosColetados, ...dados});
+  proximo();
+}
+function proximo(){
   setEtapaAtual(etapaAtual+1)
 }
 
-  return <> { formularios[etapaAtual]} </>;
+  return <>
+  <Stepper activeStep={etapaAtual}>
+    <Step><StepLabel>Login</StepLabel></Step>
+    <Step><StepLabel>Pessoal</StepLabel></Step>
+    <Step><StepLabel>entrega</StepLabel></Step>
+    <Step><StepLabel>Finalização</StepLabel></Step>
+  </Stepper>
+  { formularios[etapaAtual]} </>;
 }
 
 export default FormularioCadastro;
